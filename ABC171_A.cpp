e76c56af844f06c40ccadae991aa4b047e696a2b
onigirazu3 <onigirazu3@gmail.com>
2021-03-15 23:57:40 +0900

    Solved ABC171_A

---
+++ ABC171_A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char a;
+        cin >> a;
+        if(isupper(a)){
+            cout << "A" << endl;
+        }
+        else{
+            cout << "a" << endl;
+        }
+    return 0;
+}
