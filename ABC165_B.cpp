133f76369385b6a45aedd3943ee55c162d43c370
onigirazu3 <onigirazu3@gmail.com>
2021-04-01 15:29:14 +0900

    Solved ABC165_B

---
+++ ABC165_B.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+    long long X;
+    cin >> X;
+    long long yearA=0;
+    long long A=100;
+    while (A<X){
+        A += A/100;
+        yearA++;
+        }     
+    cout << yearA << endl;
+return 0;
+}
