f8ddccdba53d9fc7fff1535ba4dac43a14313eda
onigirazu3 <onigirazu3@gmail.com>
2021-04-03 22:58:47 +0900

    Updated ABC165_B

--- ABC165_B.cpp
+++ ABC165_B.cpp
@@ -7,12 +7,15 @@ int main()
 {
     long long X;
     cin >> X;
-    long long yearA=0;
-    long long A=100;
+    long long yearA = 0;
+    long long A = 100;
     while (A<X){
         A += A/100;
         yearA++;
-        }     
+    }
+    /*for(long long A = 100;A<X;A+=A/100){
+        yearA++;
+    }*/     
     cout << yearA << endl;
 return 0;
 }
