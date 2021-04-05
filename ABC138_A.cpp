9e5bd04fd19ae2cd23e38f4bd21419e8a7f4f95b
onigirazu3 <onigirazu3@gmail.com>
2021-04-05 22:55:06 +0900

    Solved ABC138_A

---
+++ ABC138_A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+typedef long long ll;
+int main(){
+    int a;
+    string s;
+    cin >> a;
+    cin >> s;
+    if(a>=3200){
+        cout << s << endl;
+    }
+    else{
+        cout << "red" << endl;
+    }
+}
